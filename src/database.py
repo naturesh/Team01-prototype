80ebeaf9c89ef3e0488bad83d481236437dabc2a
naturesh <altruisticsheep@icloud.com>
2025-05-16 13:18:40 +0900

    database init file

---
+++ src/database.py
@@ -0,0 +1,12 @@
+
+
+import os
+from tinydb import TinyDB
+
+
+# src/db.json 으로 데이터베이스 파일 위치 고정 
+__current_dir = os.path.dirname(os.path.abspath(__file__))
+database_file_path = os.path.join(__current_dir, "db.json")
+
+db = TinyDB(database_file_path)
+
